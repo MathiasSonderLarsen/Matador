963209a53578d0cf3c2ae086c7631f3705d8cd2b
Unknown <casperbodskov@live.dk>
2017-01-04 16:24:01 +0100

    Fleet v. 0.3 og FleetTest v. 0.4
    NOT DONE BOIZ

---
+++ src/Game/Fleet.java
@@ -0,0 +1,46 @@
+package Game;
+
+/**
+ * Keeps track of the balance, and adds/subtracts by the points on the board.
+ * <p>
+ * Bugs: none known
+ *
+ * @author Mathias S Larsen (2016)
+ * @author Casper Bodskov
+ * @author Lasse Dyrsted
+ * @author Michael Klan
+ * @author Rasmus Blichfeldt
+ * @author Timothy Rasmussen
+ * @version v.0.3
+ */
+
+public class Fleet extends Ownable {
+
+    private final int RENT_1 = 500;
+    private final int RENT_2 = 1000;
+    private final int RENT_3 = 2000;
+    private final int RENT_4 = 4000;
+
+    public Fleet(int number, String name, int price) {
+
+        super(number, name, price);
+
+    }
+
+
+    public int getRent() {
+        switch (GameController.getGameBoard().getNoOfFleetsOwned(this.getOwner())) {
+            case 1:
+                return RENT_1;
+            case 2:
+                return RENT_2;
+            case 3:
+                return RENT_3;
+            case 4:
+                return RENT_4;
+            default:
+                return 0; //Should never happen
+        }
+    }
+
+}
