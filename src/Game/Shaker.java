50fd8c357457a0d67312158f087d68ba7f493e43
Unknown <casperbodskov@live.dk>
2017-01-04 10:54:53 +0100

    Shaker v. 0.2

---
+++ src/Game/Shaker.java
@@ -0,0 +1,80 @@
+package Game;
+
+/**
+ * Shaker class, contains the Die objects
+ * <p>
+ * Bugs: none known
+ *
+ * @author Mathias S Larsen (2016)
+ * @author Casper Bodskov
+ * @author Lasse Dyrsted
+ * @author Michael Klan
+ * @author Rasmus Blichfeldt
+ * @author Timothy Rasmussen
+ * @version v.0.2
+ */
+public class Shaker {
+
+    private final Die[] dice;
+    //Initializing variables
+    private int sum;
+    private int doublesInARow=0;
+
+
+    /**
+     * Creates the Dice in the shaker
+     *
+     * @param dieCount Amount of Dice to create
+     */
+    public Shaker(int dieCount) {
+        dice = new Die[dieCount];
+        for (int i = 0; i < dieCount; i++) {
+            dice[i] = new Die();
+        }
+    }
+
+    /**
+     * Rolls all the Dice
+     */
+    public void shake() {
+        sum = 0;
+        for (Die die : this.dice) {
+            die.roll();
+            sum += die.getFaceValue();
+        }
+    }
+
+    /**
+     * Gets the sum of the rolled Dice
+     */
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    /**
+     * Gets the Dice objects in the Shaker.
+     */
+    public Die[] getDice() {
+        return dice;
+    }
+
+    /**
+     * Checks how many times in a row the Die results in doubles (1-1,2-2,3-3...)
+     */
+
+    public Die[] getDoublesInARow() { return doublesInARow; }
+
+    /**
+     * Increments the doubles in a row counter
+     */
+    public void incrementDoublesInARow(){
+       int doublesInARow = doublesInARow++;
+    }
+}
+
+
+
