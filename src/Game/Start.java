8431cb510e70bb049421b65d1ba7efe665da65ff
Shadow <lasse@dyrsted.dk>
2017-01-05 11:54:32 +0100

    Start & StartTest v0.1

---
+++ src/Game/Start.java
@@ -0,0 +1,21 @@
+package Game;
+
+/**
+ * Start class
+ * <p>
+ * Bugs: none known
+ *
+ * @author Lasse Dyrsted
+ * @version v.0.1
+ */
+public class Start extends Field {
+
+    public Start(String name) {
+        super(name);
+    }
+
+    @Override
+    public void landOnField(Player player) {
+
+    }
+}
