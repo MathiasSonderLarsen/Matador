cf61da716b162fdbc46c6c46a5ff8b78974a3829
Unknown <casperbodskov@live.dk>
2017-01-05 10:35:36 +0100

    Jail v. 0.1 og JailTest v. 0.1

---
+++ src/Game/Jail.java
@@ -0,0 +1,33 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Keeps a list of the jailed players and removes any player who gets out of jail
+ * <p>
+ * Bugs: none known
+ * @author Mathias S Larsen (2016)
+ * @author Casper Bodskov
+ * @author Lasse Dyrsted
+ * @author Michael Klan
+ * @author Rasmus Blichfeldt
+ * @author Timothy Rasmussen
+ * @version v.0.1
+ */
+
+
+public class Jail{
+    public Jail(String name){
+        super(name);
+    }
+    private ArrayList<Player> jailedPlayers;
+
+    public void landOnField(Player player) {
+    }
+
+    public boolean getIsJailed(Player player){
+        return jailedPlayers.contains(player);
+    }
+}
