909fb4d1e0e63afe775f1b21ce7cebcebe6affb2
Rasmus Blichfeldt <razzer58@gmail.com>
2017-01-05 09:04:53 +0100

    Account v.0.1 and AccountTest 0.1

---
+++ src/Game/Account.java
@@ -0,0 +1,7 @@
+package Game;
+
+/**
+ * Created by razze on 05-01-2017.
+ */
+public class Account {
+}
---
+++ src/Test/AccountTest.java
@@ -0,0 +1,7 @@
+package Test;
+
+/**
+ * Created by razze on 05-01-2017.
+ */
+public class AccountTest {
+}
