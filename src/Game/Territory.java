6178ef54cec131da22712b9d4417bbf3175cab85
Rasmus Blichfeldt <razzer58@gmail.com>
2017-01-06 11:43:49 +0100

    Territory v. 0.1

---
+++ src/Game/Territory.java
@@ -0,0 +1,64 @@
+package Game;
+
+/**
+ * Keeps track of the rent, price, name, groupID and houseprice of the territories
+ * <p>
+ * Bugs: none known
+ *
+ * @author Mathias S Larsen (2016)
+ * @author Casper Bodskov
+ * @author Lasse Dyrsted
+ * @author Michael Klan
+ * @author Rasmus Blichfeldt
+ * @author Timothy Rasmussen
+ * @version v.0.1
+ */
+
+public class Territory extends Ownable {
+
+    private int[] rentArray;
+    private int numOfHouses;
+    private int housePrice;
+
+    public Territory(String name, int price, int[] rentArray, int groupID, int housePrice) {
+        super(name, price, groupID);
+
+        this.rentArray = rentArray;
+        this.housePrice = housePrice;
+    }
+
+    public int getHousePrice() {
+        return housePrice;
+    }
+
+    public int getRent() {
+
+        switch (numOfHouses){
+
+            case 0: return rentArray[0];
+
+            case 1: return rentArray[1];
+
+            case 2: return rentArray[2];
+
+            case 3: return rentArray[3];
+
+            case 4: return rentArray[4];
+
+            case 5: return rentArray[5];
+
+            default: System.out.println("Should not happen");
+            return 0;
+        }
+
+    }
+
+    public void setNumOfHouses(int number){
+        numOfHouses = number;
+    }
+
+    public int getNumOfHouses(){
+        return numOfHouses;
+    }
+
+}
