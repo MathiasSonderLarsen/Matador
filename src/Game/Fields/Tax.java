9f9f41d860d034824f0493247c79eb2484f96b6e
Shadow <lasse@dyrsted.dk>
2017-01-13 08:38:22 +0100

    removed unused var

--- src/Game/Fields/Tax.java
+++ src/Game/Fields/Tax.java
@@ -94,7 +94,6 @@ public class Tax extends Field {
         return "Tax{" +
                 "taxAmount=" + taxAmount +
                 ", taxRate=" + taxRate +
-                ", relativeTax=" + relativeTax +
                 '}';
     }
 }
