a2cb33e6e6855a443c634838ea186cf6c25c8987
Shadow <lasse@dyrsted.dk>
2017-01-19 10:47:55 +0100

    virker

--- src/Test/FakeShaker.java
+++ src/Test/FakeShaker.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
  * Created by Matt_Lab on 10/01/2017.
  */
 public class FakeShaker extends Shaker {
-    int[] die1 = {2, 3, 6, 2, 1, 1, 3, 6, 3, 6, 2, 4, 5, 4, 1, 4, 5, 1, 4, 6, 5, 6, 5, 6, 1, 5, 2, 2, 4, 5, 6, 6, 2, 3, 6, 3, 5, 1, 2, 2, 6};
-    int[] die2 = {4, 4, 4, 2, 2, 3, 2, 6, 1, 3, 1, 6, 6, 2, 3, 3, 5, 2, 6, 5, 3, 6, 6, 6, 3, 3, 1, 2, 6, 6, 6, 6, 2, 3, 3, 6, 6, 1, 3, 2, 6};
+    int[] die1 = {1, 4, 6, 3, 5, 1, 5, 6, 3,};
+    int[] die2 = {2, 4, 6, 2, 6, 3, 6, 6, 1,};
 
     int i = 0;
 
@@ -19,8 +19,12 @@ public class FakeShaker extends Shaker {
 
     @Override
     public void shake() {
-        getDice()[0].setFaceValue(die1[i]);
-        getDice()[1].setFaceValue(die2[i++]);
+        if (i < die1.length) {
+            getDice()[0].setFaceValue(die1[i]);
+            getDice()[1].setFaceValue(die2[i++]);
+        }else {
+            super.shake();
+        }
     }
 
     @Override
